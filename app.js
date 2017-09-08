f57de2951265659a574ff90cc8c1d90ebca00cd4
Arun TE <t.e.arun1993@gmail.com>
2017-09-08 14:57:47 +0530

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Timeless first node js app')
 })
 
 app.listen(3000, function () {
