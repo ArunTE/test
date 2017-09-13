76b9df17670ea47cdc7b870b75d2c6d881a35f20
Arun TE <arun@timeless.co>
2017-09-13 17:10:45 +0530

    test data

--- app.js
+++ app.js
@@ -5,6 +5,87 @@ app.get('/', function (req, res) {
   res.send('Timeless first node js app')
 })
 
+app.get('/test', function(req, res){
+
+	var val = {
+		name: "Fazil",
+		email: "fazil@timeless",
+		projects: [
+			{
+				project_name: "BlueMatter",
+				project_description: "this is blue matter project",
+				project_id : 1,
+				task: [
+					{
+						task_name: "Login screen",
+						task_description: "build login screen",
+						task_id: 123
+					},
+					{
+						task_name: "Home screen",
+						task_description: "build home screen",
+						task_id: 1234	
+					},
+					{
+						task_name: "Next screen",
+						task_description: "build next screen",
+						task_id: 12345	
+					},
+
+				]
+			},
+			{
+				project_name: "Casino",
+				project_description: "this is casino project",
+				project_id : 2,
+				task: [
+					{
+						task_name: "Login screen",
+						task_description: "build login screen",
+						task_id: 123
+					},
+					{
+						task_name: "Home screen",
+						task_description: "build home screen",
+						task_id: 1234	
+					},
+					{
+						task_name: "Next screen",
+						task_description: "build next screen",
+						task_id: 12345	
+					},
+
+				]
+			},
+			{
+				project_name: "PacificHR",
+				project_description: "this is PacificHR project",
+				project_id : 3,
+				task: [
+					{
+						task_name: "Login screen",
+						task_description: "build login screen",
+						task_id: 123
+					},
+					{
+						task_name: "Home screen",
+						task_description: "build home screen",
+						task_id: 1234	
+					},
+					{
+						task_name: "Next screen",
+						task_description: "build next screen",
+						task_id: 12345	
+					},
+
+				]
+			}
+		]
+	};
+
+	res.send(val);
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
