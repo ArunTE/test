67ed718428ceae27970028c5ccf34fe9efc27a45
Arun TE <arun@timeless.co>
2017-09-20 16:23:20 +0530

    modified JSON

--- app.js
+++ app.js
@@ -14,76 +14,481 @@ app.get('/', function (req, res) {
 app.get('/test', function(req, res){
 
 	var val = {
-		name: "Fazil",
-		email: "fazil@timeless",
-		projects: [
+		"user_id": "123456789",
+		"email": "fazil@gmail.com",
+		"user_name": "fazil",
+		"avatar": "http://dev.timeless.co/image-path/nock/fazil.png",
+		"designation": "",
+		"time_zone": "asia-pacific",
+		"default_worksapce": "Timeless",
+		"status": "on",
+		"work_space": [
 			{
-				project_name: "BlueMatter",
-				project_description: "this is blue matter project",
-				project_id : 1,
-				task: [
+				"work_space_id": "123",
+				"name": "default",
+				"role":  "owner",
+				"settings": {
+					"avatar": "http://dev.timeless.co/image-path/nock/fazil.png",
+					"time_zone": "asia-pacific",
+					"currency": "rupee",
+					"website": "",
+				},
+				"status_flag": "active",
+				"members": [
 					{
-						task_name: "Login screen",
-						task_description: "build login screen",
-						task_id: 123
-					},
+						"user_id":"123456789",
+						"user_name": "fazil",
+						"avatar": "http://dev.timeless.co/image-path/nock/fazil.png",
+					}
+				],
+				"discussions":[],
+				"projects":[
 					{
-						task_name: "Home screen",
-						task_description: "build home screen",
-						task_id: 1234	
-					},
-					{
-						task_name: "Next screen",
-						task_description: "build next screen",
-						task_id: 12345	
-					},
+						"project_id": "",
+						"name": "",
+						"description": "",
+						"role": "",
+						"settings":{
+							"avatar": "",
+							"time_zone": "",
+							"currency": "",
+						},
+						"client_info":{
+							"name": "",
+							"email": "",
+						},
+						"privacy": "",
+						"task_email": "",
+						"status_flag": "",
+						"start_time": "",
+						"end_time": "",
+						"attachments": [
+							{
+								"file": "",
+								"annotations": "",
+							}
+						],
+						"members": [
+							{
+								"user_id": "",
+								"user_name": "",
+								"avatar": "",
+							}
+						],
+						"discussions": "",
+						"activity": "",
+						"view": "",
+						"section": [
+							{
+								"section_id": "",
+								"name": "",
+								"task":[	
+									{
+										"task_id": "",
+										"name": "",
+										"description": "",
+										"assignee":{
+											"user_id": "",
+											"user_name": "",
+											"avatar": "",
+										},
+										"due_date": "",
+										"tags": "",  
+										"priority": "",
+										"status_flag": "",
+										"privacy": "",
+										"time_log": "",
+										"members": [
+											{
+												"user_id": "",
+												"user_name": "",
+												"avatar": "",
+											}
+										],
+										"subcribers": [],
+										"discussions": "",
+										"attachments": [
+											{
+												"file": "",
+												"annotations": "",
+											}
+										],
+										"activity": "",
+										"sub_tasks":[
+											{
+												"sub_task_id": "",
+												"name": "",
+												"assignee":{
+													"user_id": "",
+													"user_name": "",
+													"avatar": "",
+												},
+												"due_date": "",
+												"status_flag": "",
+											}
+										]
+									}
 
+								]
+							}
+						]
+
+					}
 				]
 			},
 			{
-				project_name: "Casino",
-				project_description: "this is casino project",
-				project_id : 2,
-				task: [
+				"work_space_id": "9876",
+				"name": "Timeless",
+				"role":  "developer",
+				"settings": {
+					"avatar": "http://dev.timeless.co/image-path/nock/svg/timeless-logo.svg",
+					"time_zone": "asia-pacific",
+					"currency": "dollar",
+					"website": "http://bluematter.com",
+				},
+				"status_flag": "active",
+				"members": [
 					{
-						task_name: "Login screen",
-						task_description: "build login screen",
-						task_id: 123
+						"user_id":"123456789",
+						"user_name": "fazil",
+						"avatar": "http://dev.timeless.co/image-path/nock/fazil.png",
 					},
 					{
-						task_name: "Home screen",
-						task_description: "build home screen",
-						task_id: 1234	
+						"user_id":"1098853",
+						"user_name": "sandeep",
+						"avatar": "http://dev.timeless.co/image-path/nock/sandeep.png",
 					},
 					{
-						task_name: "Next screen",
-						task_description: "build next screen",
-						task_id: 12345	
-					},
+						"user_id":"53721812",
+						"user_name": "anand",
+						"avatar": "http://dev.timeless.co/image-path/nock/anand.png",
+					}
+				],
+				"discussions":["1230", "5442", "7860"],
+				"projects":[
+					{
+						"project_id": "8664328",
+						"name": "Blue Matter",
+						"description": "",
+						"role": "developer",
+						"settings":{
+							"avatar": "http://dev.timeless.co/image-path/nock/svg/bluematter-logo.svg",
+							"time_zone": "asia-pacific",
+							"currency": "dollar",
+						},
+						"client_info":{
+							"name": "john",
+							"email": "john@bluematter.com",
+						},
+						"privacy": "private",
+						"task_email": "",
+						"status_flag": "active",
+						"start_time": "12-6-2017",
+						"end_time": "12-12-2017",
+						"attachments": [
+							{
+								"file": "http://google.com",
+								"annotations": "",
+							}
+						],
+						"members": [
+							{
+								"user_id":"123456789",
+								"user_name": "fazil",
+								"avatar": "http://dev.timeless.co/image-path/nock/fazil.png",
+							},
+							{
+								"user_id":"1098853",
+								"user_name": "sandeep",
+								"avatar": "http://dev.timeless.co/image-path/nock/sandeep.png",
+							},
+							{
+								"user_id":"53721812",
+								"user_name": "anand",
+								"avatar": "http://dev.timeless.co/image-path/nock/anand.png",
+							}
+						],
+						"discussions": "1287",
+						"activity": "968647",
+						"view": "list",
+						"section": [
+							{
+								"section_id": "1324",
+								"name": "website",
+								"task":[	
+									{
+										"task_id": "64",
+										"name": "homepage",
+										"description": "",
+										"assignee":{
+											"user_id":"123456789",
+											"user_name": "fazil",
+											"avatar": "http://dev.timeless.co/image-path/nock/fazil.png",
+										},
+										"due_date": "25-9-2017",
+										"tags": "",  
+										"priority": "medium",
+										"status_flag": "active",
+										"privacy": "private",
+										"time_log": "2:00:00",
+										"members": [
+											{
+												"user_id":"123456789",
+												"user_name": "fazil",
+												"avatar": "http://dev.timeless.co/image-path/nock/fazil.png",
+											},
+											{
+												"user_id":"53721812",
+												"user_name": "anand",
+												"avatar": "http://dev.timeless.co/image-path/nock/anand.png",
+											}	
+										],
+										"subcribers": [],
+										"discussions": "32453",
+										"attachments": [
+											{
+												"file": "https://www.google.co.in/search?safe=off&q=vue+import+bootstrap+css&sa=X&ved=0ahUKEwisyJKUkLPWAhVEM48KHWPPBJYQ1QIIdCgA&biw=1440&bih=803",
+												"annotations": "",
+											},
+											{
+												"file": "https://vuejs.org/v2/guide/index.html",
+												"annotations": "",
+											},
+										],
+										"activity": "242",
+										"sub_tasks":[
+											{
+												"sub_task_id": "3232",
+												"name": "install wordpress",
+												"assignee":{
+													"user_id":"53721812",
+													"user_name": "anand",
+													"avatar": "http://dev.timeless.co/image-path/nock/anand.png",
+												},
+												"due_date": "24-9-2017",
+												"status_flag": "active",
+											},
+											{
+												"sub_task_id": "6479",
+												"name": "install theme files",
+												"assignee":{
+													"user_id":"53721812",
+													"user_name": "anand",
+													"avatar": "http://dev.timeless.co/image-path/nock/anand.png",
+												},
+												"due_date": "24-9-2017",
+												"status_flag": "active",
+											}
+										]
+									},
+									{
+										"task_id": "87",
+										"name": "retailpage",
+										"description": "",
+										"assignee":{
+											"user_id":"53721812",
+											"user_name": "anand",
+											"avatar": "http://dev.timeless.co/image-path/nock/anand.png",
+										},
+										"due_date": "29-9-2017",
+										"tags": "",  
+										"priority": "medium",
+										"status_flag": "active",
+										"privacy": "private",
+										"time_log": "2:00:00",
+										"members": [
+											{
+												"user_id":"123456789",
+												"user_name": "fazil",
+												"avatar": "http://dev.timeless.co/image-path/nock/fazil.png",
+											},
+											{
+												"user_id":"53721812",
+												"user_name": "anand",
+												"avatar": "http://dev.timeless.co/image-path/nock/anand.png",
+											}	
+										],
+										"subcribers": [],
+										"discussions": "67997",
+										"attachments": [
+											{
+												"file": "https://www.google.co.in/search?safe=off&q=vue+import+bootstrap+css&sa=X&ved=0ahUKEwisyJKUkLPWAhVEM48KHWPPBJYQ1QIIdCgA&biw=1440&bih=803",
+												"annotations": "",
+											},
+											{
+												"file": "https://vuejs.org/v2/guide/index.html",
+												"annotations": "",
+											},
+										],
+										"activity": "870",
+										"sub_tasks":[
+											{
+												"sub_task_id": "5786",
+												"name": "install plugin",
+												"assignee":{
+													"user_id":"123456789",
+													"user_name": "fazil",
+													"avatar": "http://dev.timeless.co/image-path/nock/fazil.png",
+												},
+												"due_date": "27-9-2017",
+												"status_flag": "active",
+											},
+											{
+												"sub_task_id": "6479",
+												"name": "install other files",
+												"assignee":{
+													"user_id":"123456789",
+													"user_name": "fazil",
+													"avatar": "http://dev.timeless.co/image-path/nock/fazil.png",
+												},
+												"due_date": "27-9-2017",
+												"status_flag": "active",
+											}
+										]
+									}
 
-				]
-			},
-			{
-				project_name: "PacificHR",
-				project_description: "this is PacificHR project",
-				project_id : 3,
-				task: [
-					{
-						task_name: "Login screen",
-						task_description: "build login screen",
-						task_id: 123
-					},
-					{
-						task_name: "Home screen",
-						task_description: "build home screen",
-						task_id: 1234	
-					},
-					{
-						task_name: "Next screen",
-						task_description: "build next screen",
-						task_id: 12345	
-					},
+								]
+							},
+							{
+								"section_id": "5688",
+								"name": "socialmedia",
+								"task":[	
+									{
+										"task_id": "87",
+										"name": "homepage",
+										"description": "",
+										"assignee":{
+											"user_id":"123456789",
+											"user_name": "fazil",
+											"avatar": "http://dev.timeless.co/image-path/nock/fazil.png",
+										},
+										"due_date": "25-9-2017",
+										"tags": "",  
+										"priority": "medium",
+										"status_flag": "active",
+										"privacy": "private",
+										"time_log": "2:00:00",
+										"members": [
+											{
+												"user_id":"123456789",
+												"user_name": "fazil",
+												"avatar": "http://dev.timeless.co/image-path/nock/fazil.png",
+											},
+											{
+												"user_id":"53721812",
+												"user_name": "anand",
+												"avatar": "http://dev.timeless.co/image-path/nock/anand.png",
+											}	
+										],
+										"subcribers": [],
+										"discussions": "32453",
+										"attachments": [
+											{
+												"file": "https://www.google.co.in/search?safe=off&q=vue+import+bootstrap+css&sa=X&ved=0ahUKEwisyJKUkLPWAhVEM48KHWPPBJYQ1QIIdCgA&biw=1440&bih=803",
+												"annotations": "",
+											},
+											{
+												"file": "https://vuejs.org/v2/guide/index.html",
+												"annotations": "",
+											},
+										],
+										"activity": "242",
+										"sub_tasks":[
+											{
+												"sub_task_id": "3232",
+												"name": "install wordpress",
+												"assignee":{
+													"user_id":"53721812",
+													"user_name": "anand",
+													"avatar": "http://dev.timeless.co/image-path/nock/anand.png",
+												},
+												"due_date": "24-9-2017",
+												"status_flag": "active",
+											},
+											{
+												"sub_task_id": "6479",
+												"name": "install theme files",
+												"assignee":{
+													"user_id":"53721812",
+													"user_name": "anand",
+													"avatar": "http://dev.timeless.co/image-path/nock/anand.png",
+												},
+												"due_date": "24-9-2017",
+												"status_flag": "active",
+											}
+										]
+									},
+									{
+										"task_id": "87",
+										"name": "retailpage",
+										"description": "",
+										"assignee":{
+											"user_id":"53721812",
+											"user_name": "anand",
+											"avatar": "http://dev.timeless.co/image-path/nock/anand.png",
+										},
+										"due_date": "29-9-2017",
+										"tags": "",  
+										"priority": "medium",
+										"status_flag": "active",
+										"privacy": "private",
+										"time_log": "2:00:00",
+										"members": [
+											{
+												"user_id":"123456789",
+												"user_name": "fazil",
+												"avatar": "http://dev.timeless.co/image-path/nock/fazil.png",
+											},
+											{
+												"user_id":"53721812",
+												"user_name": "anand",
+												"avatar": "http://dev.timeless.co/image-path/nock/anand.png",
+											}	
+										],
+										"subcribers": [],
+										"discussions": "67997",
+										"attachments": [
+											{
+												"file": "https://www.google.co.in/search?safe=off&q=vue+import+bootstrap+css&sa=X&ved=0ahUKEwisyJKUkLPWAhVEM48KHWPPBJYQ1QIIdCgA&biw=1440&bih=803",
+												"annotations": "",
+											},
+											{
+												"file": "https://vuejs.org/v2/guide/index.html",
+												"annotations": "",
+											},
+										],
+										"activity": "870",
+										"sub_tasks":[
+											{
+												"sub_task_id": "5786",
+												"name": "install plugin",
+												"assignee":{
+													"user_id":"123456789",
+													"user_name": "fazil",
+													"avatar": "http://dev.timeless.co/image-path/nock/fazil.png",
+												},
+												"due_date": "27-9-2017",
+												"status_flag": "active",
+											},
+											{
+												"sub_task_id": "6479",
+												"name": "install other files",
+												"assignee":{
+													"user_id":"123456789",
+													"user_name": "fazil",
+													"avatar": "http://dev.timeless.co/image-path/nock/fazil.png",
+												},
+												"due_date": "27-9-2017",
+												"status_flag": "active",
+											}
+										]
+									}
 
+								]
+							}
+						]
+
+					}
 				]
 			}
 		]
